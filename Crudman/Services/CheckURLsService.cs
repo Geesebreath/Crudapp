27b2606d242b53ebfc93f8c06b3d2fbb7faf725c
Nathan Giesbrecht <47060952+Geesebreath@users.noreply.github.com>
2026-01-21 00:05:15 -0700

    Setup initial webrequest test
    Set base execution to serverside via InteractiveServer rendermode, set up service script and test with local page.

---
+++ Crudman/Services/CheckURLsService.cs
@@ -0,0 +1,36 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+namespace Crudman.Services;
+
+public class CheckURLsService
+{
+    private bool urlResponseError;
+
+    public async Task Check(string urlString,IHttpClientFactory clientFactory )
+    {
+        Console.WriteLine("Check Service Started");
+        using var request = new HttpRequestMessage(HttpMethod.Get,
+            urlString);
+        //request.Headers.Add("Accept", "application/vnd.github.v3+json");
+        //request.Headers.Add("User-Agent", "HttpClientFactory-Sample");
+
+        var client = clientFactory.CreateClient();
+
+        using var response = await client.SendAsync(request);
+
+        if (response.IsSuccessStatusCode)
+        {
+            using var responseStream = await response.Content.ReadAsStreamAsync();
+            Console.WriteLine("GET success");
+            //branches = await JsonSerializer.DeserializeAsync<IEnumerable<GitHubBranch>>(responseStream);
+        }
+        else
+        {
+            urlResponseError = true;
+            Console.WriteLine("GET failure");
+        }
+        Console.WriteLine("Check Service finished");
+    }
+
+
+}
